376f54285efb7082ea81b0d012dddb0b3f214047
Anton Holmberg <anton.holmberg@cloudberrypine.com>
2025-07-25 18:21:35 +0200

    Add non-recursive version of copy bundle resources

--- hello_imgui_cmake/assets/him_assets_apple_bundle.cmake
+++ hello_imgui_cmake/assets/him_assets_apple_bundle.cmake
@@ -13,6 +13,25 @@ function(hello_imgui_apple_bundle_add_files_from_folder app_name assets_folder l
     endforeach()
 endfunction()
 
+function(hello_imgui_apple_bundle_add_files_from_folder_non_recursive app_name assets_folder location_in_bundle)
+    file(GLOB assets ${assets_folder}/*.*)
+    target_sources(${app_name} PRIVATE ${assets})
+    foreach(asset ${assets})
+        file(RELATIVE_PATH asset_relative ${assets_folder} ${asset})
+        get_filename_component(asset_dir ${asset_relative} DIRECTORY)
+        set_source_files_properties(
+                ${asset}
+                PROPERTIES
+                MACOSX_PACKAGE_LOCATION ${location_in_bundle}/${asset_dir}
+        )
+    endforeach()
+endfunction()
+
 function(hello_imgui_bundle_assets_from_folder app_name assets_folder)
     hello_imgui_apple_bundle_add_files_from_folder(${app_name} ${assets_folder} "Resources/assets")
+endfunction()
+
+
+function(hello_imgui_bundle_assets_from_folder_non_recursive app_name assets_folder)
+    hello_imgui_apple_bundle_add_files_from_folder_non_recursive(${app_name} ${assets_folder} "Resources/assets")
 endfunction()
